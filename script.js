5df618a94cbde7ca047977fe945cee4183b05bc2
vellumgracia-del <vellumgracia@gmail.com>
2025-10-07 12:00:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+// 1. Pilih tombol berdasrakan ID-nya
+const tombolSapa = document.getElementById('sapaButton');
+// 2. Tambahkan 'event listener' unttuk aksi 'click'
+tombolSapa.addEventListener('click', function(){
+  // 3. Jalankan kode ini ketika tombol di-klik
+  alert('Halo! Selamat Datang Di Website Kami!');
+});
